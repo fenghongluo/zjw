f17307fa53e9e53acd7c9a1a32d9afb963bc58d2
fenghongluo <790990581@qq.com>
2018-12-27 15:51:21 +0800

    添加了一个类

---
+++ zjw/src/main/java/zjw/testgit.java
@@ -0,0 +1,11 @@
+package zjw;
+
+public class testgit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("wo yao bei shang chuan zhi  git");
+
+	}
+
+}
